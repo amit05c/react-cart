af30382e1f41a3fabb2acc3d145f502d6b7b2fb9
amig05c <amitghosh0595c@gmail.com>
2022-06-17 19:52:54 +0530

    Men's page,home,men's data page are only working

---
+++ src/pages/Cupon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Button, Flex,} from '@chakra-ui/react'
+
+const Cupon = () => {
+  return (
+    <div>
+        <Box backgroundColor={"blue"} width={"100%"} height={"5vh"}>
+            <h3>10% off on every product</h3>
+        </Box>
+    </div>
+  )
+}
+
+export default Cupon
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,93 @@
 import React from 'react'
-import { Box, Flex,} from '@chakra-ui/react'
+import { Box, Button, Flex, Grid, Image,} from '@chakra-ui/react'
+import { PhoneIcon, AddIcon, WarningIcon, ChatIcon, ExternalLinkIcon } from '@chakra-ui/icons'
+import Cupon from './Cupon'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 
 const Home = () => {
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    let getData= async ()=>{
+      const res= await axios("https://fakestoreapi.com/products?limit=10")
+      // console.log(res.data)
+      setData(res.data)
+    }
+    getData()
+  },[])
+  console.log(data)
   return (
     <div>
+      <Cupon/>
       <Flex flexDirection={"column"}>
      <Box backgroundColor={"gray"} width={"100%"} height={"100vh"}>
-
+      
+       <Grid  templateColumns='repeat(5, 1fr)'
+        templateRows='repeat(2, 1fr)'  
+       >
+      {data.map(elem=>(
+        <div key={elem.id}>
+       <NavLink to={"/manage"}>  <Image   src={elem.image} width={"100%"} />  </NavLink>
+         </div>
+      ))}
+         </Grid>
+       
      </Box>
 
-     <Box bg={"tomato"}>
-      
+     <Box bg={"tomato"} height={"50vh"}>
+      <Flex justifyContent={"space-evenly"}>
+        <div>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        </div>
+
+
+        <div>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        </div>
+
+
+        <div>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        </div>
+
+
+        <div>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        <p>item</p>
+        </div>
+
+      </Flex >
+      <Box style={{margin:"50px"}}>
+        <h2>contact us</h2>
+        <Flex justifyContent={"center"} gap={"10px"}>
+        <PhoneIcon />
+        <ChatIcon />
+        <ExternalLinkIcon />
+        <Button colorScheme='twitter' > Twitter</Button>
+        </Flex>
+        
+        
+      </Box>
      </Box>
      </Flex>
         
